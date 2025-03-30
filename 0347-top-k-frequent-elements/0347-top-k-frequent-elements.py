7a0eb48b9487521321e1bccbcc0c6757ef41600f
Kunal Mahato <mahatokunal41@gmail.com>
2025-03-29 22:04:16 -0400

    Time: 7 ms (51.36%), Space: 24.1 MB (19.67%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,15 +1,15 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        n = 100000
-        freq = {}
-        for num in nums:
-            freq[num] = freq.get(num, 0) + 1
+        n = len(nums)
+        # freq = {}
+        # for num in nums:
+        #     freq[num] = freq.get(num, 0) + 1
+        freq = Counter(nums)
         num_buckets = n+1
         buckets = [[] for _ in range(num_buckets)]
         for key, value in freq.items():
             buckets[value].append(key)
         finalList = []
-        print(buckets)
         for subList in reversed(buckets):
             if subList:
                 for element in  subList:

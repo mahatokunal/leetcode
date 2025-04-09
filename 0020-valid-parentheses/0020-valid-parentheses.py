f63da00cf7ad6d6e102afecce5972f6e8528f514
Kunal Mahato <mahatokunal41@gmail.com>
2025-04-09 00:13:37 -0400

    Time: 0 ms (100%), Space: 17.8 MB (80.95%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -5,16 +5,12 @@ class Solution:
         for ch in s:
             if ch in b_map:
                 if not stack:
-                    # print('a')
                     return False
                 else:
                     top=stack.pop()
                     if b_map[ch]!=top:
-                        # print('b')
-                        # print('ch = ', ch , 'b_map[ch] = ', b_map[ch], "top = ", top)
                         return False
             else:
-                # print('c')
                 stack.append(ch)
         if stack:
             return False

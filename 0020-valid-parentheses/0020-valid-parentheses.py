5e32155b0ca48675fed3491f408dd60c4d368b93
Kunal Mahato <mahatokunal41@gmail.com>
2025-04-09 00:08:23 -0400

    Time: 0 ms (100%), Space: 17.9 MB (51.03%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,18 +1,22 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        b_map = {')':'(', '}':'{', ']':'['}
         stack = []
-        for i in range(0, len(s)):
-            if ( s[i] == '(' or s[i] == '{' or s[i] == '['):
-                stack.append(s[i])
+        for ch in s:
+            if ch in b_map:
+                if not stack:
+                    # print('a')
+                    return False
+                else:
+                    top=stack.pop()
+                    if b_map[ch]!=top:
+                        # print('b')
+                        # print('ch = ', ch , 'b_map[ch] = ', b_map[ch], "top = ", top)
+                        return False
             else:
-                if len(stack)==0:
-                    return False
-                top=stack.pop()
-                ord_diff = ord(s[i])-ord(top)
-                if ord_diff!=1 and  ord_diff!=2:
-                    return False
-        if len(stack)==0:
-            return True
-        else:
+                # print('c')
+                stack.append(ch)
+        if stack:
             return False
+        return True
 

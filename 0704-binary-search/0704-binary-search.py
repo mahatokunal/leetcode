efd3a5c2414b630927336d0262da2f7381d359ff
Kunal Mahato <mahatokunal41@gmail.com>
2025-04-28 21:48:53 -0400

    Time: 0 ms (100%), Space: 18.6 MB (73.78%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -3,12 +3,10 @@ class Solution:
         l, r = 0, len(nums)-1
         while l<=r:
             mid = (l+r)//2
-            print("mid = ", mid)
             if nums[mid]==target:
                 return mid
             elif nums[mid]>target:
                 r= mid-1
             else:
-                print("c")
                 l=mid+1
         return -1

d1f0a84e3cf1a338ab884c75ee482490c380f6f2
Kunal Mahato <mahatokunal41@gmail.com>
2025-06-05 20:15:33 -0400

    Time: 3 ms (42.4%), Space: 17.8 MB (38.03%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,18 +1,18 @@
 class Solution:
-    def getDigitsArray(self, num:int):
-        return [int(d) for d in str(abs(num)) if d.isdigit()]
+    def nextNumber(self, n: int) -> int:
+        total = 0
+        while n>0:
+            d = n%10
+            total += d*d
+            n = n//10
+        return total
 
     def isHappy(self, n: int) -> bool:
-        hashSet = set()
-        new_num=n
-        while(True):
-            hashSet.add(new_num)
-            digits = self.getDigitsArray(new_num)
-            if len(digits)==1 and digits[0]==1:
-                return True
-            else:
-                new_num = 0
-                for digit in digits:
-                    new_num += (digit*digit)
-                if new_num in hashSet:
-                    return False
+        slow = n
+        fast = self.nextNumber(n)
+
+        while fast!=1 and fast!=slow:
+            slow = self.nextNumber(slow)
+            fast = self.nextNumber(self.nextNumber(fast))
+        
+        return fast==1

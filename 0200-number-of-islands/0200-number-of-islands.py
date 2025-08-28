fe0d95d1cbd456128032156382527c82668d7199
Kunal Mahato <mahatokunal41@gmail.com>
2025-08-27 23:19:38 -0400

    Time: 234 ms (83.15%), Space: 20.3 MB (58.21%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,28 +1,36 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         ans=0
-        v_set = set()
+        # v_set = set()
         rows, cols = len(grid), len(grid[0])
         
         def dfs(i, j):
-            if j+1<cols and grid[i][j+1]=="1" and (i, j+1) not in v_set:
-                v_set.add((i, j+1))
-                dfs(i, j+1)
-            if j-1>-1 and grid[i][j-1]=="1" and (i, j-1) not in v_set:
-                v_set.add((i, j-1))
-                dfs(i, j-1)
-            if i+1<rows and grid[i+1][j]=="1" and (i+1, j) not in v_set:
-                v_set.add((i+1, j))
-                dfs(i+1, j)
-            if i-1>-1 and grid[i-1][j]=="1" and (i-1, j) not in v_set:
-                v_set.add((i-1, j))
-                dfs(i-1, j)
+            if i<0 or i>=rows or j<0 or j>=cols or grid[i][j]=="0":
+                return
+            grid[i][j]="0"
+            dfs(i, j+1)
+            dfs(i, j-1)
+            dfs(i+1, j)
+            dfs(i-1, j)
+            # if j+1<cols and grid[i][j+1]=="1" and (i, j+1) not in v_set:
+            #     v_set.add((i, j+1))
+            #     dfs(i, j+1)
+            # if j-1>-1 and grid[i][j-1]=="1" and (i, j-1) not in v_set:
+            #     v_set.add((i, j-1))
+            #     dfs(i, j-1)
+            # if i+1<rows and grid[i+1][j]=="1" and (i+1, j) not in v_set:
+            #     v_set.add((i+1, j))
+            #     dfs(i+1, j)
+            # if i-1>-1 and grid[i-1][j]=="1" and (i-1, j) not in v_set:
+            #     v_set.add((i-1, j))
+            #     dfs(i-1, j)
             return 
 
         for i in range(rows):
             for j in range(cols):
-                if grid[i][j]=="1" and (i, j) not in v_set:
-                    v_set.add((i, j))
+                if grid[i][j]=="1":
+                    #  and (i, j) not in v_set:
+                    # v_set.add((i, j))
                     dfs(i, j)
                     ans+=1
         return ans
